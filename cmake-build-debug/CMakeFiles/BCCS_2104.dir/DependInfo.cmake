b70fa5ba2b12bb7b6c72177664531fa4828e593d
richa1200 <richa.987.gupta@gmail.com>
2020-12-20 21:22:45 +0530

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/BCCS_2104.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/I_am_/OneDrive/Desktop/OOP Project/2019-BCS-047_project/bill.cpp" "C:/Users/I_am_/OneDrive/Desktop/OOP Project/2019-BCS-047_project/cmake-build-debug/CMakeFiles/BCCS_2104.dir/bill.cpp.obj"
+  "C:/Users/I_am_/OneDrive/Desktop/OOP Project/2019-BCS-047_project/item.cpp" "C:/Users/I_am_/OneDrive/Desktop/OOP Project/2019-BCS-047_project/cmake-build-debug/CMakeFiles/BCCS_2104.dir/item.cpp.obj"
+  "C:/Users/I_am_/OneDrive/Desktop/OOP Project/2019-BCS-047_project/main.cpp" "C:/Users/I_am_/OneDrive/Desktop/OOP Project/2019-BCS-047_project/cmake-build-debug/CMakeFiles/BCCS_2104.dir/main.cpp.obj"
+  "C:/Users/I_am_/OneDrive/Desktop/OOP Project/2019-BCS-047_project/reception.cpp" "C:/Users/I_am_/OneDrive/Desktop/OOP Project/2019-BCS-047_project/cmake-build-debug/CMakeFiles/BCCS_2104.dir/reception.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/BCCS_2104.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "BCCS_2104.exe"
+  "BCCS_2104.exe.manifest"
+  "BCCS_2104.pdb"
+  "CMakeFiles/BCCS_2104.dir/bill.cpp.obj"
+  "CMakeFiles/BCCS_2104.dir/item.cpp.obj"
+  "CMakeFiles/BCCS_2104.dir/main.cpp.obj"
+  "CMakeFiles/BCCS_2104.dir/reception.cpp.obj"
+  "libBCCS_2104.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BCCS_2104.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
